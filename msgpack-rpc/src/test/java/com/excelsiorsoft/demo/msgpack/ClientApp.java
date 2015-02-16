27b68cebba5c12e6054cf30717b0ae92a2909dcf
sleyzerzon <senya22@yahoo.com>
2015-02-15 19:12:23 -0500

    project to play with what msgpack has to offer

---
+++ msgpack-rpc/src/test/java/com/excelsiorsoft/demo/msgpack/ClientApp.java
@@ -0,0 +1,52 @@
+package com.excelsiorsoft.demo.msgpack;
+
+import org.msgpack.rpc.Client;
+import org.msgpack.rpc.loop.EventLoop;
+
+public class ClientApp {
+	
+	private static class SpawnRequest {
+		
+		private SpawnRequest(final int clientCount, final int requestCount, final RPCInterface iface) {
+			
+			new Thread(new Runnable() {
+				public void run() {
+					for (int rc = 1; rc <= requestCount; rc++) {
+						System.out.println("(" + clientCount + ":" + rc	+ ") 10 + 2 = " + iface.add(10, 2));
+						System.out.println("(" + clientCount + ":" + rc + ") 10 - 2 = " + iface.sub(10, 2));
+						System.out.println("(" + clientCount + ":" + rc	+ ") 10 * 2 = " + iface.mul(10, 2));
+						System.out.println("(" + clientCount + ":" + rc	+ ") 10 / 2 = " + iface.div(10, 2));
+					}
+				}
+			}).start();
+		}
+	}
+
+	public static interface RPCInterface {
+		
+		int add(int a, int b);
+
+		int sub(int a, int b);
+
+		int mul(int a, int b);
+
+		double div(int a, int b);
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		EventLoop loop = EventLoop.defaultEventLoop();
+		
+		Client client = new Client("localhost", 1978, loop);
+		
+		RPCInterface iface = client.proxy(RPCInterface.class);
+		
+		final int CLIENT_COUNT = 2;
+		final int REQUEST_COUNT = 1;
+		
+		System.out.println("Spawning " + CLIENT_COUNT + " clients to perform " + REQUEST_COUNT + " requests each");
+		for (int cc = 1; cc <= CLIENT_COUNT; cc++) {
+			new SpawnRequest(cc, REQUEST_COUNT, iface);
+		}
+	}
+}
---
+++ msgpack-rpc/src/test/java/com/excelsiorsoft/demo/msgpack/MathServer.java
@@ -0,0 +1,32 @@
+package com.excelsiorsoft.demo.msgpack;
+
+import org.msgpack.rpc.Server;
+import org.msgpack.rpc.loop.EventLoop;
+
+
+public class MathServer {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int mul(int a, int b) {
+		return a * b;
+	}
+
+	public double div(int a, int b) {
+		return a / b;
+	}
+
+	public static void main(String[] args) throws Exception {
+		EventLoop loop = EventLoop.defaultEventLoop();
+		Server svr = new Server();
+		svr.serve(new MathServer());
+		svr.listen(1978);
+		System.out.println("MathServer listening on 1978");
+		loop.join();
+	}
+}
